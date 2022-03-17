2b3f0c150159dddc6e1fe82882332818852c13ec
tanln <tan_ln@163.com>
2022-03-17 15:51:57 +0800

    Updates

---
+++ precache-manifest.7a47ca45fb07bddf1118122343165769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c361c17940ac824eda2",
+    "url": "/ele-static-page/static/css/main.a9f925f6.chunk.css"
+  },
+  {
+    "revision": "5c361c17940ac824eda2",
+    "url": "/ele-static-page/static/js/main.4fd19708.chunk.js"
+  },
+  {
+    "revision": "f09f9af5dbb6c1f008e3",
+    "url": "/ele-static-page/static/js/runtime~main.c2475d31.js"
+  },
+  {
+    "revision": "c71af5f1dea031191d97",
+    "url": "/ele-static-page/static/js/2.28e83ac4.chunk.js"
+  },
+  {
+    "revision": "8c2f9a49921564f9cd0138fee63b5b7d",
+    "url": "/ele-static-page/static/media/default_icon.8c2f9a49.jpeg"
+  },
+  {
+    "revision": "586ab69a702ef5e77ca705a6e7c5736a",
+    "url": "/ele-static-page/index.html"
+  }
+];
